3cd1e1ed4bd80d4e5186ad0d81e6e209a4938b92
Nguyen Anh Duy <aduy1912@gmail.com>
2023-05-15 10:16:04 +0700

    Presentational and Container Components

--- src/App.js
+++ src/App.js
@@ -1,32 +1,14 @@
-// import logo from './logo.svg';
 import './App.css';
-import Navigation from './components/Navigation'
-import Players from './components/Players'
-import Footer from './components/Footer'
-
+import Footer from './components/Footer';
+import Main from './components/Main';
+import Navigation from './components/Navigation';
 function App() {
   return (
-    <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello, I am Nadariano!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-
+    <div className='App'>
       <Navigation/>
-      <Players/>
+      <Main/>
       <Footer/>
     </div>
   );
 }
-
 export default App;
